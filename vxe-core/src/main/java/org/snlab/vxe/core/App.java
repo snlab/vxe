4f690298c21a3047d54d304c0cd71af56fed4b7a
Kai Gao <godrickk@gmail.com>
2016-04-03 10:22:01 +0800

    Configure checkstyle plugin
    Signed-off-by: Kai Gao <godrickk@gmail.com>

--- vxe-core/src/main/java/org/snlab/vxe/core/App.java
+++ vxe-core/src/main/java/org/snlab/vxe/core/App.java
@@ -4,8 +4,10 @@ package org.snlab.vxe.core;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    int ABC;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
